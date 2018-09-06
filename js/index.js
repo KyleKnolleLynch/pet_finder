64db5bbfa7e43942ad7708a4daa16560e83967d4
Kyle Lynch <kylelynch104@gmail.com>
2018-09-05 20:41:44 -0700

    version_1.0.0

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ function fetchAnimals(e) {
   }
 
   fetchJsonp(
-    `http://*api.petfinder.com/pet.find?format=json&key=ce2c5a56ca95ba4f28cad1a4eac1a7a2&animal=${animal}&location=${zip}&callback=callback`,
+    `http://api.petfinder.com/pet.find?format=json&key=ce2c5a56ca95ba4f28cad1a4eac1a7a2&animal=${animal}&location=${zip}&callback=callback`,
     {
       jsonpCallbackFunction: 'callback'
     }
